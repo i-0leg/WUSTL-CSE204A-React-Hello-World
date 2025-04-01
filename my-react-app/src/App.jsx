5068ee984e6d221dcb4dcfdddf46a9cb0d04385b
oi <oi@MIMIMI-COMP>
2025-04-01 17:01:06 -0500

    corrected error

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -8,8 +8,6 @@ function Square({ value, onSquareClick  }) {
 }
 
 function Board({ xIsNext, squares, onPlay }) {
-    const [xIsNext, setXIsNext] = useState(true);
-    const [squares, setSquares] = useState(Array(9).fill(null));
 
     function handleClick(i) {
         if (calculateWinner(squares) || squares[i]) {
