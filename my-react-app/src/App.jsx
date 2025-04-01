0da7c50a145d99500dc959413573ca6407da9308
oi <oi@MIMIMI-COMP>
2025-04-01 16:54:20 -0500

    I HAVE A BEAUTIFUL WINNER CALCULATOR I MADE FOR 332 BUT IG THIS IS FINE FOR NOW ;C

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -47,4 +47,24 @@ export default function Board() {
             </div>
         </>
     );
+
+    function calculateWinner(squares) { //I HAVE A BEAUTIFUL WINNER CALCULATOR I MADE FOR 332 BUT IG THIS IS FINE FOR NOW ;C
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
 }
