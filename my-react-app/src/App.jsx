12904cee638fbf26e5ae3b767e7ca453c7adeab6
oi <oi@MIMIMI-COMP>
2025-04-01 16:58:27 -0500

    added game compomemt over everythimg

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -7,7 +7,7 @@ function Square({ value, onSquareClick  }) {
         </button>);
 }
 
-export default function Board() {
+function Board() {
     const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -56,24 +56,37 @@ export default function Board() {
             </div>
         </>
     );
+}
 
-    function calculateWinner(squares) { //I HAVE A BEAUTIFUL WINNER CALCULATOR I MADE FOR 332 BUT IG THIS IS FINE FOR NOW ;C
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6]
-        ];
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
-            }
+export default function Game() {
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+            <div className="game-info">
+                <ol>{/*TODO*/}</ol>
+            </div>
+        </div>
+    );
+}
+
+function calculateWinner(squares) { //I HAVE A BEAUTIFUL WINNER CALCULATOR I MADE FOR 332 BUT IG THIS IS FINE FOR NOW ;C
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
         }
-        return null;
     }
+    return null;
 }
